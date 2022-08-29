33775cbe200981300ae440aa9578919641e13945
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-29 18:50:06 +0300

    Create Task4.java

---
+++ ItMath/Task4.java
@@ -0,0 +1,10 @@
+public class Task4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print("A");
+            }
+            System.out.println();
+        }
+    }
+}
