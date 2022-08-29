5905747bfddb612ac410743672b248120c598cec
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-29 18:48:06 +0300

    Create Task3.java

---
+++ ItMath/Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args) {
+        String x = "0";
+        for (int i = 0; i < 5; i++) {
+            System.out.println(x);
+            x += "0";
+        }
+    }
+}
