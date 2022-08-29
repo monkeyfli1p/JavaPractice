99a0c0a396f5a49ab57cc93ffbc187e21d006ad8
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-29 19:04:29 +0300

    Create Task5.java

---
+++ ItMath/Task5.java
@@ -0,0 +1,8 @@
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.println("*     *     *\n" +
+                           " *   * *   * \n" +
+                           "  * *   * *  \n" +
+                           "   *     *   ");
+    }
+}
