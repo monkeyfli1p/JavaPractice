f14a2b8ab1de5d64d423fd04770ca90d60049554
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-30 19:09:00 +0300

    Create Task14.java

---
+++ ItMath/SimpleArithmetic/Task14.java
@@ -0,0 +1,11 @@
+public class Task14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a * b = " + (a * b));
+    }
+}
