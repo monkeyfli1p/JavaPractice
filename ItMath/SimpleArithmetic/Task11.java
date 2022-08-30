92566fbaaf92801b0e30ff8c25501ace94631f01
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-30 18:44:18 +0300

    Create Task11.java

---
+++ ItMath/SimpleArithmetic/Task11.java
@@ -0,0 +1,7 @@
+public class Task11 {
+    public static void main(String[] args) {
+        double x = -2.34;
+
+        System.out.println(((Math.abs(x - 5) - Math.sin(x)) / 3) + (Math.sqrt(Math.pow(x, 2) + 2014) * Math.cos(2 * x) - 3));
+    }
+}
