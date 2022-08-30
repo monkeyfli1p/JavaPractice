64e7a2276f29b1f63075e071c38002eefd99a26b
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-30 21:35:40 +0300

    Create Task18.java
    The user enters the side of the square. Find the perimeter and area of the square.

---
+++ ItMath/SimpleArithmetic/Task18.java
@@ -0,0 +1,10 @@
+public class Task18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+
+        System.out.println("Perimeter = " + (x * 4));
+        System.out.println("Area = " + (x * x));
+    }
+}
