e1481cf50455455ff6dd20216709aa263240185f
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-30 20:07:17 +0300

    Create Task17.java

---
+++ ItMath/SimpleArithmetic/Task17.java
@@ -0,0 +1,15 @@
+public class Task16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        a = a * 2;
+        b = b - 3;
+        c = c * c;
+
+        System.out.println(a + b + c);
+    }
+}
