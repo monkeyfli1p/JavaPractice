2cf6cc8f4abb498496d995e96c2ea25e4be9d00a
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-30 19:02:32 +0300

    Create Task12.java

---
+++ ItMath/SimpleArithmetic/Task12.java
@@ -0,0 +1,7 @@
+public class Task12 {
+    public static void main(String[] args) {
+        double x = 3.6;
+
+        System.out.println(Math.pow(Math.E, x - 2) + Math.abs(Math.sin(x)) - Math.pow(x, 4) * Math.cos(1 / x));
+    }
+}
