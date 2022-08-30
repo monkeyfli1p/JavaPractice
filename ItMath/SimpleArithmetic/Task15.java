2a70ff8a14d331d1a8dc21bd627bc7c94befb802
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-08-30 19:11:13 +0300

    Create Task15.java
    The user enters a number. Display the square of that number, the cube of that number.

---
+++ ItMath/SimpleArithmetic/Task15.java
@@ -0,0 +1,10 @@
+public class Task15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+
+        System.out.println("x * x = " + (Math.pow(x, 2)));
+        System.out.println("x * x * x = " + (Math.pow(x, 3)));
+    }
+}
