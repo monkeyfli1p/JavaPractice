0714b8d2253d1a4a2066cd340fadb795f1832b0c
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-09-04 12:20:15 +0300

    Create Task31.java
    A rectangle with a size of 647 x 170 is given. How many squares with a side of 30 can be cut out of it?

---
+++ ItMath/SimpleArithmetic/Task31.java
@@ -0,0 +1,11 @@
+public class Task31 {
+    public static void main(String[] args) {
+        int a = 647;
+        int b = 170;
+
+        int Pab = (a * b);
+        int Psquare = 30 * 30;
+
+        System.out.println(Pab / Psquare);
+    }
+}
