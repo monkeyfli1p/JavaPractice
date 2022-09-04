1f4876b946be09e04eea6dafbc26647b12a40382
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-09-04 13:06:24 +0300

    Create Task37.java
    Two numbers are given. Output the larger of the two numbers.

---
+++ ItMath/SimpleArithmetic/ConditionalStatement/Task37.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task37 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int x1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+
+        System.out.println(Math.max(x1, x2));
+    }
+}
