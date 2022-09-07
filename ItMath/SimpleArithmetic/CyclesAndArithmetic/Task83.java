c106262c89c4055ab63feec6296e6e8d0ca9faae
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-09-07 13:36:16 +0300

    Create Task83.java
    Output the following lines. First: 25 25.5 24.8. Second: 26 26.5 25.8. And so on. Last line: 35 35.5 34.8.

---
+++ ItMath/SimpleArithmetic/CyclesAndArithmetic/Task83.java
@@ -0,0 +1,7 @@
+public class Task83 {
+    public static void main(String[] args) {
+        for (int i = 25; i <= 35; i++) {
+            System.out.println(i + " " + ((double)i + 0.5) + " " + ((double)i - 0.2));
+        }
+    }
+}
