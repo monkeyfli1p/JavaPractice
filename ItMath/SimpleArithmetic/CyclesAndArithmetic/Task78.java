53dcf524202c64d2a98236bcac13894092fd4f47
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-09-07 13:13:06 +0300

    Create Task78.java
    Display a figure of asterisks on the screen:
    *******
    *******
    *******
    *******
    (a square of n lines, each line contains n asterisks)

---
+++ ItMath/SimpleArithmetic/CyclesAndArithmetic/Task78.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task78 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        reader.close();
+    }
+}
