cc6e84a198d7293a9984ad557b8bd20f3056ef27
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-09-07 13:40:46 +0300

    Create Task84.java
    The user enters the dollar exchange rate in roubles. Show a table with the prices of $1, $2, ..., $100 in roubles, and in the third column add the number of candies you can buy with these amounts, if the price of 1 kg of candy is 20 roubles. Example: 1$ - 70p - 3.5kg and so on (100 rows in total).

---
+++ ItMath/SimpleArithmetic/CyclesAndArithmetic/Task84.java
@@ -0,0 +1,7 @@
+public class Task84 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + "$ - " + (i * 70) + "rub - " + ((double)(i * 70) / 20) + "kg.");
+        }
+    }
+}
