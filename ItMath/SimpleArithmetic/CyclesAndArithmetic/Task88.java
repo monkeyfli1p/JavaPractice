cb8fd2fd9ab773b5a593a968a9ffddd11c1c72b9
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-09-07 13:58:08 +0300

    Create Task88.java
    Find the product of 5⋅6⋅7⋅...⋅13.

---
+++ ItMath/SimpleArithmetic/CyclesAndArithmetic/Task88.java
@@ -0,0 +1,11 @@
+public class Task88 {
+    public static void main(String[] args) {
+        int sum = 1;
+
+        for (int i = 5; i <= 13; i++) {
+            sum *= i;
+        }
+
+        System.out.println(sum);
+    }
+}
