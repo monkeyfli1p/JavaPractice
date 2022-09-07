ba9785c26455b2deddd1bbc62a4387ababc6f74b
monkeyfli1p <111335882+monkeyfli1p@users.noreply.github.com>
2022-09-07 13:56:48 +0300

    Create Task87.java
    Find the sum of 10+11+12+13+...+88.

---
+++ ItMath/SimpleArithmetic/CyclesAndArithmetic/Task87.java
@@ -0,0 +1,11 @@
+public class Task87 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 10; i <= 88; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
